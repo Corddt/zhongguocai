f39189bebe40d558f1ec9ecbeaae3ea6af7a6697
Corddt <3304484088@qq.com>
2023-06-28 22:03:47 +0800

    资源页面新增导航栏，再次优化了鼠标单击图片后的显示事件。修改了一些bug。

--- src/main/resources/static/js/tupian_ziyuan.js
+++ src/main/resources/static/js/tupian_ziyuan.js
@@ -9,15 +9,41 @@ for (var i = 0; i < images.length; i++) {
     images[i].onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
+        modal.classList.add('show'); // 添加这一行
     }
 }
 
 // 为关闭按钮添加点击事件
 span.onclick = function() {
     modal.style.display = "none";
+    modal.classList.remove('show'); // 添加这一行
 }
 
 // 点击大图的任意位置也可以关闭
 modal.onclick = function() {
     modal.style.display = "none";
+    modal.classList.remove('show'); // 添加这一行
 }
+
+//这是导航栏的js
+window.onload = function () {
+    var menuItems = document.querySelectorAll('.menu ul li');
+    var slider = document.getElementById('slider');
+
+    // 设置初始位置
+    setSliderPosition(document.getElementById('home'));
+
+    // 对每个菜单项添加事件监听
+    menuItems.forEach(item => {
+        item.addEventListener('mouseenter', function () {
+            setSliderPosition(item);
+        });
+    });
+
+    function setSliderPosition(item) {
+        var rect = item.getBoundingClientRect();
+        slider.style.left = rect.left + 'px';
+        slider.style.width = rect.width + 'px';
+    }
+}
+
