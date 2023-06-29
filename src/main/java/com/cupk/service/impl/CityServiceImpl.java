7ad242a5d41639d2b920c0ecc31da4250959515e
Corddt <3304484088@qq.com>
2023-06-29 17:40:40 +0800

    在城市的service实现类中添加了service注解。

--- src/main/java/com/cupk/service/impl/CityServiceImpl.java
+++ src/main/java/com/cupk/service/impl/CityServiceImpl.java
@@ -4,6 +4,8 @@ import com.cupk.mapper.CityMapper;
 import com.cupk.pojo.City;
 import com.cupk.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ import java.util.List;
  * @author:zjf
  * @datatime:2023-06-29 17:16
  */
+@Service
+@Primary
 public class CityServiceImpl implements CityService {
     @Autowired(required = false)
     private CityMapper cityMapper;
