8157e34884e2ad0b962e2dab96a2d30560ddd5df
Corddt <3304484088@qq.com>
2023-06-30 11:52:07 +0800

    新增了传承人的前后端调用，新增传承人的分页查询前端页面。

---
+++ src/main/java/com/cupk/pojo/ChuanChengRen.java
@@ -0,0 +1,23 @@
+package com.cupk.pojo;
+
+import lombok.Data;
+
+/**
+ * 名称:ChuanChengRen
+ * 描述:传承人的实体类
+ *
+ * @version 1.0
+ * @author:zjf
+ * @datatime:2023-06-30 11:27
+ */
+@Data
+public class ChuanChengRen {
+    private Integer id;
+    private String xingming;
+    private String xingbie;
+    private String minzu;
+    private String shenfen;
+    private String caixi;
+    private String caiming;
+    private String diqu;
+}
